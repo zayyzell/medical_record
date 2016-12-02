76f40bae373e3c399c76cdc01e4de2b803f37502
Rodrigo Perez <rodrigoperezanton@hotmail.com>
2016-12-02 12:18:37 -0500

    testing

--- spec/features/user_can_register_in_the_apps_spec.rb
+++ spec/features/user_can_register_in_the_apps_spec.rb
@@ -1,30 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanRegisterInTheApps", type: :feature do
+
+
   let!(:user) do
-    User.create(
-    email: "testing@wyncode.co",
-    password: "12345"
-    confirm_password: "12345"
-    )
+    # User.create(
+    # email: "testing@wyncode.co",
+    # password: "12345",
+    # confirm_password: "12345"
+    # )
   end
   it "generates a new user" do
     #go to signup page
     visit root_path
 
     #fill email field
-    fill_in "Email", with "testing@wyncode.co"
+    fill_in "Email", with: "testing@wyncode.co"
     #fill password
-    fill_in "Password", with "12345"
-
-    #fill password_confirmation
-    fill_in "confirm_password", with "12345"
+    fill_in "Password", with: "12345"
 
     #submit to create new user
     click_button "Create User"
     #confirm user was created
   ["testing@wyncode.co", '12345'].each do |text|
-    expect (page).to have_content text
+    expect(page).to have_content text
   end
     #confirm user has blank medical record
   end
